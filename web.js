e9267ea002bedb35957dbc2bff9b127ab151b5bf
Jason Victor <jasonmvictor@gmail.com>
2013-12-01 03:46:45 +0000

    Updated web.js to pull from index.html

--- web.js
+++ web.js
@@ -3,7 +3,11 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+
+  var buffer = new Buffer(fs.readFile('index.html'));
+  var strBuffer = buffer.toString("utf-8");
+
+  response.send(strBuffer);
 });
 
 var port = process.env.PORT || 5000;
