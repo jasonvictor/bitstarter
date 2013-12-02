1ed2f6b232661e643e64f0742c0824551752c311
Jason Victor <jasonmvictor@gmail.com>
2013-12-02 05:12:22 +0000

    final version

--- grader.js
+++ grader.js
@@ -23,6 +23,7 @@ References:
 */
 
 var fs = require('fs');
+var rest = require('restler');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
@@ -31,7 +32,7 @@ var CHECKSFILE_DEFAULT = "checks.json";
 var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
-	console.log("%s does not exist.  Existing.", instr);
+	console.log("%s does not exist.  Exiting.", instr);
 	process.exit(1);  // http://nodejs.org/api/process.html#process_process_exit_code
     }
     return instr;
@@ -55,6 +56,17 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     }
     return out;
 };
+// too much duplicated code here -- need more efficient way
+var checkURL = function(urlContents, checksfile) {
+    $ = cheerio.load(urlContents);
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for (var ii in checks) {
+	var present = $(checks[ii]).length > 0;
+	out[checks[ii]] = present;
+    }
+    return out;
+};
 
 var clone = function(fn) {
     //Workaround for commander.js issue
@@ -66,9 +78,29 @@ if(require.main == module) {
     program
     .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
     .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+    .option('-u, --url <url_to_file>', 'URL to index.html')
     .parse(process.argv);
 
-    var checkJson = checkHtmlFile(program.file, program.checks);
+    //if a url is specified, process this instead of the file.
+    if (program.url) {
+
+	//Get the URL's contents in string form
+	rest.get(program.url).on('complete', function(result) {
+
+if (result instanceof Error) {
+    console.log('Error: ' + result.message);
+} else {
+    // there's a better way to do this than duplicating code
+    var checkJson  = checkURL(result, program.checks);
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
+}
+	});
+
+    }
+    else {
+	var checkJson = checkHtmlFile(program.file, program.checks);
+    }
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
